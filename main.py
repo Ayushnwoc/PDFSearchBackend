4ea1a4a0183b3fcce5447cc5cc2ce7169e5ed9de
Ayush kumar <ayush.kumar@tailwnd.com>
2024-06-30 18:13:21 +0530

    upgrade cors again

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods (GET, POST, PUT, DELETE, etc.)
     allow_headers=["*"],  # Allows all headers
